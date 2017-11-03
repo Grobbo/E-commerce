4d288b0dadb1018579c902aabc4933cbe5029f00
Grobbo <suneri-5@student.ltu.se>
2017-11-03 13:12:19 +0100

    button test

--- index.php
+++ index.php
@@ -1,6 +1,24 @@
-<HTML>
-<HEAD>
-</HEAD>
-<BODY>
-	<P> "HELLLLLLLLLL YEAH!" </P>
+<html>
+<head>
+ <head>
+<link rel="stylesheet" type="text/css" href="style.css">
+</head> 
+
+</head>
+<body>
+ 	<div class="dropdown">
+	<button class="dropbtn">Könisar</button>
+		<div class="dropdown-content">
+			 <a href="#">Syfilis</a>
+   		 	<a href="#">Gonorre</a>
+   		 	<a href="#">Condylom</a>
+  		</div class="dropdown">
+	</div>
+	<div class="dropdown">
+	<button class="dropbtn">Devs</button>
+                  <div class="dropdown-content">
+                	 <a href="#">Erik Sundström</a>
+                 	<a href="#">Ludvig Isaksson</a>
+                </div class="dropdown">
+	</div>
 </BODY>
