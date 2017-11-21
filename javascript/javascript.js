9db9d569d418b3b1a6d2f24a773a720e7eafe6dc
Grobbo <suneri-5@student.ltu.se>
2017-11-21 12:03:01 +0100

    mer fixes

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -18,15 +18,15 @@ function itemize_list(list){
  
 	var string = build_top_menu();
 	for(index = 0;index < list.length; ++index){
-		string += '<div class="item_text"> <p>' + list[index].manufacturer+ '</div>' +
+		string += '<div class="item_container" onclick="say_hello('+list[index].id+')"' + list[index].id + '>' + 
+		'<div class="item_text"> <p>' + list[index].manufacturer+ '</div>' + 
 		'<div class="item_text"> <p>' + list[index].category + '</div>' +
 		'<div class="item_text"> <p>' + list[index].description + '</div>' +
 		'<div class="item_text"> <p>' + list[index].price + '</div>' +
-		build_add_to_cart_button(list[index].id)+ '</div>'; 
+		build_add_to_cart_button(list[index].id)+ '</div><br>'; 
 	}
 	return string;		
 }
-
 
 function build_top_menu(){
 	return '<div class="item_div"><div class="item_text"><p>Manufacturer</p></div><div class="item_text"><p>Category</p></div><div class="item_text"><p>Description</p></div><div class="item_text"><p>Price</p></div></div>';
@@ -35,5 +35,9 @@ function build_top_menu(){
 
 //need to be processed
 function build_add_to_cart_button (id){
-	return '<div class="item_text"><form action="php/cart.php" method="POST"><input name="product_id" type="hidden" value="' + id +'"/><button class="add_button" type="submit">Add to Cart</button></form></div><br>';
+	return '<div class="item_text"><form action="php/cart.php" method="POST"><input name="product_id" type="hidden" value="' + id +'"/><button class="add_button" type="submit">Add to Cart</button></form></div>';
+}
+
+function say_hello(id){
+	console.log("Item ID "+ id);
 }
