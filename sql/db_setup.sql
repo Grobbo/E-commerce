e241c3b21e0622970b65b63633bce4bb18702266
ludisa-5 <ludvig.isaksson@gmail.com>
2017-11-30 12:27:57 +0100

    asd

--- sql/db_setup.sql
+++ sql/db_setup.sql
@@ -83,36 +83,41 @@ INSERT INTO E_COMMERCE.PRODUCTS(
 	manufacturer,
 	description,
 	quantity,
-	price)
+	price,
+	rating)
  VALUES ('SAW',
 	'MAKITA',
 	'USED FOR HEAVY-DUTY SAWING',
 	7,
-	250);
+	250,
+	4.5);
 
 INSERT INTO E_COMMERCE.PRODUCTS(
 	category,
 	manufacturer,
 	description,
 	quantity,
-	price)
+	price,
+	rating)
  VALUES ('SAW',
 	'COCRAFT',
 	'BASIC SAW',
 	3,
-	89);
+	89,
+	4);
 
 INSERT INTO E_COMMERCE.PRODUCTS(
 	category,
 	manufacturer,
 	description,
 	quantity,
-	price)
+	price,rating)
  VALUES ('SAW',
 	'BACHO',
 	'METAL SAW',
 	22,
-	349);
+	349,
+	5);
 /*
 
 */
@@ -121,12 +126,14 @@ INSERT INTO E_COMMERCE.PRODUCTS(
 	manufacturer,
 	description,
 	quantity,
-	price)
+	price,rating
+	)
  VALUES ('SAW',
 	'MISC',
 	'METAL SAW',
 	15,
-	200);
+	200,
+	1);
 
 /* SCREWDRIVERS */
 
