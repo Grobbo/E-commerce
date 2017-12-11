5dd73b0f8c66a0164c0a93e9f645970d7213dca9
Ludvigis <Ludvig.isaksson@gmail.com>
2017-12-11 17:56:43 +0100

    checkout

--- php/checkout.php
+++ php/checkout.php
@@ -10,31 +10,26 @@ if($_SERVER["REQUEST_METHOD"] == "GET"){
 
 function checkout($user_id,$num_items){
 	$con = db_connect();
-	mysqli_begin_transaction($con, MYSQLI_TRANS_START_READ_WRITE);		
-	$sql1 = "INSERT INTO SHIPMENTS (order_date,customer) values (NOW(),'$user_id')";
+	//mysqli_begin_transaction($con, MYSQLI_TRANS_START_READ_WRITE);		
 	
-	$sql2 = "INSERT INTO ORDERS (product_id,quantity)
-					SELECT product_id,quantity FROM SHOPPING_CART WHERE user_id = '$user_id';
-			INSERT INTO ORDERS (shipment_id) values ('$shipment_id')";
-	$sql3 = "DELETE FROM SHOPPING_CART WHERE user_id = '$user_id';";
-	
-	mysqli_query($con,$sql1);
+	$sql1 = "INSERT INTO SHIPMENTS (order_date,customer) values (NOW(),'$user_id')";	
+	mysqli_query($con,$sql1);						//create new shipment...
 	$shipment_id = mysqli_insert_id($con);
+	$sql2 = "SELECT product_id FROM SHOPPING_CART WHERE user_id = '$user_id'";
+	$result = mysqli_query($con,$sql2);		//all products in shopping cart
+	$products = mysqli_fetch_all($result,MYSQLI_ASSOC);	
 	
+	$sql3 = "INSERT INTO ORDERS (product_id,quantity,shipment_id) values";
 	
-	/*mysqli_query($con,$sql2);
-	if(mysqli_affected_rows($con)<=0){//an error occured or no rows affected
-		print(mysqli_error($con));
-	}else{							//query ok
-		print("Hej");
+	for($i=0;$i<count($products);$i++){
+		$product_id = intval($products[$i]['product_id']);
+		$sql3 .= "('$product_id',1,'$shipment_id'),";
 	}
-	//mysqli_query($con,$sql3);
-	if(!mysqli_commit($con)){
-		//print("Transaction failed!");
-	}else{
-		//print("Success!");
-	}
-	*/
+	$sql3 = chop($sql3 , ",");
+
+	mysqli_query($con,$sql3);
+	$sql4 = "DELETE FROM SHOPPING_CART WHERE user_id = '$user_id';";
+	mysqli_query($con,$sql4);
 }
 
 function db_connect(){
