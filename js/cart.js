19ce463204c18982637ef833168e98d968510161
ludisa-5 <ludvig.isaksson@gmail.com>
2017-12-14 08:29:45 +0100

    cart

--- js/cart.js
+++ js/cart.js
@@ -41,8 +41,7 @@ function checkout(){
 	var xhttp = new XMLHttpRequest();
 	xhttp.onreadystatechange = function() {
 		if (this.readyState == 4 && this.status == 200) { 
-			//var rjson = JSON.parse(this.responseText);
-			alert(this.responseText);
+			alert("Checkout Succesful");
 			getCurrentCart();
 		}
 	}
